aeafc4920686e2e3ae0323cb263536f134b2ca4e
gamingbuddhist <68414004+gamingbuddhist@users.noreply.github.com>
2022-05-26 17:50:37 -0400

    fixed a few more doc bugs

--- docs/conf.py
+++ docs/conf.py
@@ -17,12 +17,12 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'import-python'
+project = 'import-python-web'
 copyright = '2022, Christopher James'
 author = 'Christopher James'
 
 # The full version, including alpha/beta/rc tags
-release = '2.1.0'
+release = '1.0.3'
 
 
 # -- General configuration ---------------------------------------------------
